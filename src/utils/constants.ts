1072be3b5e918e9588bbb98cffd51dde1cb8150e
vhcsilva <vhcsilvaa@gmail.com>
2024-06-17 16:50:54 -0300

    create a constant to manage game layout by difficult

---
+++ src/utils/constants.ts
@@ -0,0 +1,17 @@
+export const GameLayout = {
+  beginner: {
+    cols: 9,
+    rows: 9,
+    mines: 10,
+  },
+  intermediate: {
+    cols: 16,
+    rows: 16,
+    mines: 40,
+  },
+  advanced: {
+    cols: 24,
+    rows: 24,
+    mines: 99,
+  },
+}
